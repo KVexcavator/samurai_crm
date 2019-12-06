943366f92d701bd1389aa4ab1f0f7344a60a340e
excavator <str_token@mail.ru>
2019-12-06 18:13:34 +0300

    Create admin users controller

--- engines/core/app/controllers/samurai/admin/admin_controller.rb
+++ engines/core/app/controllers/samurai/admin/admin_controller.rb
@@ -1,9 +1,11 @@
 module Samurai
   module Admin
     class AdminController < ApplicationController
+
       def index
-        
+        @users = Samurai::User.ordered
       end
+
     end
   end
 end
