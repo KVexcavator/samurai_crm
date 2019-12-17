4399795573b02e1cba64630f9532945526810ffd
excavator <str_token@mail.ru>
2019-12-17 16:28:12 +0300

    update dummy? dont run rspec> need migrate

--- engines/core/spec/dummy/config/application.rb
+++ engines/core/spec/dummy/config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "core"
+require "samurai/core"
 
 module Dummy
   class Application < Rails::Application
