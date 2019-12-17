1c7a7bcec677bce1a66df2acfe15552558d3cb8b
excavator <str_token@mail.ru>
2019-12-17 17:04:43 +0300

    add test & delete old db

--- engines/core/spec/models/samurai/user_spec.rb
+++ engines/core/spec/models/samurai/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-# module Samurai
-#   describe User do
-#     it "has a valid factory" do
-#       expect(FactoryGirl.build(:user)).to be_valid
-#     end
-#   end
-# end
+module Samurai
+  describe User do
+    it "has a valid factory" do
+      expect(FactoryGirl.build(:user)).to be_valid
+    end
+  end
+end
